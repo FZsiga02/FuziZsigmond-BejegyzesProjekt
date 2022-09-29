f9eb97261f154e9e386cca845a760e5f3d554576
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-09-29 08:30:37 +0200

    List

--- src/hu/petrik/oop/Main.java
+++ src/hu/petrik/oop/Main.java
@@ -1,8 +1,19 @@
 package hu.petrik.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    private static List<Bejegyzes> bejegyzesek = new ArrayList<>();
+
     public static void main(String[] args) {
+
+        Bejegyzes bejegyzes = new Bejegyzes("Kiss István", "Valami tartalom");
+        Bejegyzes bejegyzes2 = new Bejegyzes("Gipsz Jakab", "Ez is tartalom");
+
+        bejegyzesek.add(bejegyzes);
+        bejegyzesek.add(bejegyzes2);
 
     }
 }
